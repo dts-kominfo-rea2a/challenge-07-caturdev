fa780f37a66736db8be4f9da0776dcd65b06327a
catur wicaksono <catur.wicaksono.developer@gmail.com>
2022-06-12 20:52:29 +0700

    Tantangan: Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,34 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (category) => {
+  try {
+
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    let good = 0;
+    let bad = 0;
+
+    theaterIXX.forEach((data) => {
+      if (data.hasil === 'marah') bad += 1;
+      if (data.hasil === 'tidak marah') good += 1;
+    });
+
+    theaterVGC.forEach((data) => {
+      if (data.hasil === 'marah') bad += 1;
+      if (data.hasil === 'tidak marah') good += 1;
+    });
+
+    if (category === 'marah') return bad;
+    if (category === 'tidak marah') return good;
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
+};
 
 module.exports = {
   promiseOutput,
